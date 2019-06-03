65a8a23cee5776ead16e1cab5c2e67fa31abe43e
Sergey_Gavrilov <sergey.gavrilov@inplatlabs.ru>
2019-06-03 10:36:40 +0300

    Added link list (dict) for imports

--- main.py
+++ main.py
@@ -6,13 +6,15 @@ if __name__ == '__main__':
     mac_dir = '/Users/sergeygavrilov/PycharmProjects/ids_fssp'
     venv_dir = '/home/newander/PycharmProjects/etl_scheduler/dags'
 
-    project = Path(venv_dir)
+    project = Path(mac_dir)
     report_dest = Path.cwd() / 'result.csv'
 
     parser = Parser(project)
+
     parser.extract_tree()
-    parser.build_tree()
+    parser.gather_objects()
+    parser.build_link_list()
 
     print(parser)
-    parser.root.modules[7].get_imports()
+    # parser.root.modules[7].get_imports()
     # parser.create_report(report_dest)
