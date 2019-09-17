9695d629bbea1129949c06ed58aea4ec1efb116d
newander <gavrilovsa@pik.ru>
2019-09-17 23:37:29 +0300

    Added static settings for graph

--- src/drawer.py
+++ src/drawer.py
@@ -3,30 +3,55 @@ from collections import namedtuple
 from pyvis import network as net
 
 from src.linker import Linker
+from src.tree import Module
 
 
 class GraphManager:
     """ Needed to create and draw graphs """
+    height = 1000  # pixels
+    width = 1000  # pixels
 
     def __init__(self, linker: Linker):
         self.linker = linker
 
     def create_import_graph(self):
+        """ Creates graph from import objects in Linker """
         Edge = namedtuple('Edge', 'from_ to_')
-        graph = net.Network(notebook=True)
+        graph = net.Network(
+            height=f'{self.height}px',
+            width=f'{self.width}px',
+        )
 
         for lib, descr in self.linker.items():
             edges_lst = []
-            edges_new = {lib}
+            new_libs = set()
+            new_modules = {lib}
             for import_ in descr['imports']:
-                to_module = str(import_['module'])
-                edges_lst.append(Edge(lib, to_module))
-                edges_new.add(to_module)
+                module = import_['module']
 
-            graph.add_nodes(edges_new)
+                if isinstance(module, Module):
+                    to_edge = module.abs_import
+                    new_modules.add(to_edge)
+                else:
+                    to_edge = str(module)
+                    new_libs.add(to_edge)
+
+                edges_lst.append(Edge(lib, to_edge))
+
+            graph.add_nodes(
+                list(new_libs),
+                color=['#DBE129'] * len(new_libs)
+            )
+            graph.add_nodes(
+                list(new_modules),
+                color=['blue'] * len(new_modules)
+            )
             graph.add_edges(edges_lst)
 
         return graph
 
     def save(self, graph: net.Network, path: str):
+        """ Save any graph (with possible options) """
+        # For debugging graph view
+        graph.show_buttons(filter_=['nodes'])
         graph.save_graph(path)
